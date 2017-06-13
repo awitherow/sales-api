9368a8e8b1e0a9e91434785f09799b32d82bfbb3
awitherow <au.witherow@gmail.com>
2017-06-13 14:41:19 +0200

    chore(scripts): enable serverless.yml generation

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Create a .env with at minimum
 ```bash
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
+export USER_POOL_ARN=
+export AWS_REGION=
 ```
 
 source this when completed.
@@ -14,3 +16,7 @@ source this when completed.
 ```bash
 source .env
 ```
+
+## deployment
+
+Before any form of deployment, make sure you have your environment variables properly set up and then run the script found at `scripts/setup_serverless_yml.sh`.
---
+++ scripts/setup_serverless_yml.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# please run this script from the root of the project,
+# else many of the commands will fail
+
+source .env
+
+echo "Preparing serverless.yml"
+echo
+
+USER_POOL_ARN=$USER_POOL_ARN
+AWS_REGION=$AWS_REGION
+
+# https://stackoverflow.com/a/525612/1475153
+sed 's~$USER_POOL_ARN~'"$USER_POOL_ARN"'~g' serverless-template.yml
+sed 's~$AWS_REGION~'"$AWS_REGION"'~g' serverless-template.yml
+
+mv serverless-template.yml serverless.yml
+
+echo "All environment variables successfully transferred"
+exit
