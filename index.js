fc9b0ff2cc6cbe9b9d740cb72392ba3c26a37221
awitherow <au.witherow@gmail.com>
2017-06-13 14:44:45 +0200

    feat(products): functionality to add product to dynamoDB

---
+++ index.js
@@ -0,0 +1,45 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  region: "us-west-2"
+});
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "PRODUCTS_TABLE",
+    Item: {
+      userID: event.requestContext.authorizer.claims.sub,
+      productID: uuid.v1(),
+      createdAt: new Date().getTime()
+    }
+  };
+
+  dynamoDB.put(params, (err, data) => {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    };
+
+    const response = {
+      headers
+    };
+
+    if (err) {
+      return callback(null, {
+        ...response,
+        statusCode: 500,
+        body: JSON.stringify({ status: false })
+      });
+    }
+
+    return callback(null, {
+      ...response,
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    });
+  });
+}
